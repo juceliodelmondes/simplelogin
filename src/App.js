f6a3260001effe765c7a24cea35f43d733d8daea
Jucelio Delmondes <jucelio.2013@hotmail.com>
2020-07-27 03:16:43 -0300

    semWarnings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import ReactDOM from 'react-dom';
 import './App.css';
 
 import {DivPrincipal, CardLogin, DivParticles, CardCriarConta} from './styles/styles'
